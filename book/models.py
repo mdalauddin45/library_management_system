75584e6e864b53970d5deab841c37f7b2d03b094
Md Ala Uddin <92620825+mdalauddin45@users.noreply.github.com>
2023-12-28 23:23:12 +0600

    Book collect and creaate

--- book/models.py
+++ book/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from category.models import Category
 
 # Create your models here.
+class Book(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    image = models.ImageField(upload_to='media/')
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+ 
+    def __str__(self):
+        return self.title
+class UserReviews(models.Model):
+    post = models.ForeignKey(Book, on_delete=models.CASCADE, related_name='comments')
+    name = models.CharField(max_length=50)
+    email = models.EmailField()
+    body = models.TextField()
+    created_on= models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return f"Review by {self.name}"
