7afa5ec994001172e6a2f17715c8fdb188552492
Md Ala Uddin <92620825+mdalauddin45@users.noreply.github.com>
2023-12-29 00:44:10 +0600

    profile purches table add

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,11 +1,10 @@
 
 from django.urls import path
-from .views import UserRegistrationView, UserLoginView, UserLogoutView,UserProfileView,profile
+from .views import UserRegistrationView, UserLoginView, UserLogoutView,ProfileView
  
 urlpatterns = [
     path('register/', UserRegistrationView.as_view(), name='register'),
     path('login/', UserLoginView.as_view(), name='login'),
     path('logout/', UserLogoutView.as_view(), name='logout'),
-    # path('profile/', UserProfileView.as_view(), name='profile' )
-    path('profile/', profile, name='profile' )
+    path('profile/', ProfileView.as_view(), name='profile' )
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -9,7 +9,8 @@ from django.contrib.auth.forms import SetPasswordForm
 from django.contrib.auth.views import LoginView, LogoutView
 from django.core.mail import EmailMultiAlternatives
 from django.template.loader import render_to_string
-
+from django.views.generic import ListView
+from book.models import Purchase
 # Create your views here.
 class UserRegistrationView(FormView):
     template_name = 'accounts/user_registration.html'
@@ -35,21 +36,10 @@ class UserLogoutView(LogoutView):
             logout(self.request)
         return reverse_lazy('home')
 
-class UserProfileView(View):
+class ProfileView(ListView):
     template_name = 'accounts/profile.html'
+    context_object_name = 'data'
 
-    # def get(self, request):
-    #     form = UserUpdateForm(instance=request.user)
-    #     return render(request, self.template_name, {'form': form})
-
-    # def post(self, request):
-    #     form = UserUpdateForm(request.POST, instance=request.user)
-    #     if form.is_valid():
-    #         form.save()
-    #         return redirect('profile')  # Redirect to the user's profile page
-    #     return render(request, self.template_name, {'form': form})
-
-def profile(request):
-    # user_purchases = Purchase.objects.filter(user=request.user)
-    # data = [purchase.car for purchase in user_purchases]
-    return render(request, 'accounts/profile.html')
+    def get_queryset(self):
+        user_purchases = Purchase.objects.filter(user=self.request.user)
+        return [purchase.book for purchase in user_purchases]
