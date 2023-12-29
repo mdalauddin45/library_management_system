882e0cac4e583d5c165c4bdb7e96d1f10f20c0c9
Md Ala Uddin <92620825+mdalauddin45@users.noreply.github.com>
2023-12-29 12:30:20 +0600

    git ignore file added

--- library_management_system/settings.py
+++ library_management_system/settings.py
@@ -11,16 +11,20 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import environ
+
+env = environ.Env()
+environ.Env.read_env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
+SECRET_KEY = env("SECRET_KEY")
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-!k(%fco_&g%0p-(iw6@37l2%^6tb0@%7$z&((7jq^a#&r&dy%*'
+# SECRET_KEY = 'django-insecure-!k(%fco_&g%0p-(iw6@37l2%^6tb0@%7$z&((7jq^a#&r&dy%*'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -136,3 +140,9 @@ MEDIA_ROOT = BASE_DIR / 'media'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_USE_TLS = True
+EMAIL_PORT = 587
+EMAIL_HOST_USER = env("EMAIL")
+EMAIL_HOST_PASSWORD = env("EMAIL_PASSWORD")
--- transactions/views.py
+++ transactions/views.py
@@ -5,7 +5,18 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic import CreateView,ListView
 from django.urls import reverse_lazy
 from .forms import DepositForm
+from django.core.mail import EmailMultiAlternatives
+from django.template.loader import render_to_string
 # Create your views here.
+def send_transaction_email(user,amount,subject,template):
+    message = render_to_string(template,{
+        'user': user,
+        'amount':amount,
+    })
+    send_email = EmailMultiAlternatives(subject, '',to=[user.email])
+    send_email.attach_alternative(message, "text/html")
+    send_email.send()
+
 class TransactionCreateMixin(LoginRequiredMixin, CreateView):
     template_name = 'transactions/transaction_form.html'
     model = Transaction
@@ -41,6 +52,7 @@ class DepositMoneyView(TransactionCreateMixin):
             self.request,
             f'{"{:,.2f}".format(float(amount))}$ was deposited to your account successfully'
         )
+        send_transaction_email(self.request.user,amount,"Deposit Message", 'transactions/deposit_email.html' )
         return super().form_valid(form)
 
 class TransactionReportView(LoginRequiredMixin, ListView):
