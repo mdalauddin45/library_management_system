822886919bd8b680778b64ccfa2d4bc8c72e66a0
Md Ala Uddin <92620825+mdalauddin45@users.noreply.github.com>
2023-12-30 11:43:24 +0600

    logout not warking

--- library_management_system/urls.py
+++ library_management_system/urls.py
@@ -3,10 +3,12 @@ from django.urls import path,include
 from core.views import HomeView
 from django.conf import settings
 from django.conf.urls.static import static
+from accounts.views import UserLogoutView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', HomeView.as_view(), name='home'),
+    path('logout/', UserLogoutView.as_view(), name='logout'),
     path('home/<slug:category_slug>/', HomeView.as_view(), name='category_wise_post'),
     path('accounts/', include('accounts.urls')),
     path('transactions/', include('transactions.urls')),
